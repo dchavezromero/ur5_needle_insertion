77fd22872b6039706bdba39755368dbcabb857de
Dennis Chavez Romero <dennis.chavezromero@hotmail.com>
2025-02-20 16:46:39 +0100

    Busted moveitconfig.

---
+++ ur5_robot_description/launch/ur5_moveit.launch.py
@@ -0,0 +1,25 @@
+import os
+from sys import prefix
+import yaml
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
+
+def generate_launch_description():
+    moveit_config = (
+        MoveItConfigsBuilder("ur5")
+        .robot_description(file_path="config/ur5.urdf.xacro")
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .planning_scene_monitor(
+            publish_robot_description=True, publish_robot_description_semantic=True
+        )
+        .planning_pipelines(pipelines=["ompl"]) # maybe change to RRTStar
+        .to_moveit_configs()
+    )
+
+    
